44757b45f5eb08f4d072c0fff74619f600ebd26a
Django Cass <dj.cass44@gmail.com>
2019-09-10 22:39:01 +1000

    Added TypeScript support

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import * as React from "react";
 import {MuiThemeProvider} from "@material-ui/core";
 import Body from "./containers/Body";
+import theme from "./theme";
 
 export default () => (
 	<div className="App">
-		<MuiThemeProvider>
+		<MuiThemeProvider theme={theme}>
 			<Body/>
 		</MuiThemeProvider>
 	</div>
